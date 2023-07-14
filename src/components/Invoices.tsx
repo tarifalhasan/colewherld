1b010b786402000ab815c6809f24620409198748
tarifalhasan <tarialhasan999@gmail.com>
2023-07-14 20:05:06 +0600

    update mobile responsive

--- src/components/Invoices.tsx
+++ src/components/Invoices.tsx
@@ -17,9 +17,9 @@ const Invoices = () => {
           {invoincesData.map((data, index) => (
             <li
               key={index}
-              className=" px-5  bg-primary shadow-large rounded-[10px] flex justify-between items-center "
+              className=" px-1 sm:px-5  bg-primary shadow-large rounded-[10px] flex justify-between items-center "
             >
-              <div className="p-3 font-medium capitalize">
+              <div className="p-3 max-w-[55%] font-medium capitalize">
                 <div className="  flex items-center gap-3 py-2 font-normal  ">
                   <div className="relative h-10 w-10">
                     <div className="  w-10 bg-[#36333D] h-10 grid place-items-center shadow-base">
@@ -27,15 +27,19 @@ const Invoices = () => {
                     </div>
                   </div>
 
-                  <h3 className="   font-medium text-lg">{data.title}</h3>
+                  <h3 className="   font-medium text-xs sm:text-lg">
+                    {data.title}
+                  </h3>
                 </div>
               </div>
 
-              <div className=" space-x-4">
-                <button className="  py-2 rounded-full  btn_primary">
+              <div className=" space-x-2 sm:space-x-4">
+                <button className=" text-[10px]  sm:text-base py-1 sm:py-2 rounded-full  btn_primary">
                   View
                 </button>
-                <button className="btn_secondary rounded-full">Download</button>
+                <button className=" text-[10px]  rounded-full sm:text-base   border text-DodgerBlue font-medium border-DodgerBlue px-3 sm:px-5 py-2">
+                  Download
+                </button>
               </div>
             </li>
           ))}
